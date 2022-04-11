6a90b4e2b935b7bc992ac3384f0758d667f42b9a
Denis Uhanov <udenis80@mail.ru>
2022-04-11 22:45:27 +0300

    spring

--- ClassicalMusic.java
+++ ClassicalMusic.java
@@ -4,6 +4,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClassicalMusic implements Music {
     public String getSong() {
-        return "Hungarian rapsody";
+        return "Hungarian";
     }
 }
